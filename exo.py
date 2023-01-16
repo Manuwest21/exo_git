04f678d3d650cb1d6b2a8629e637255ca4e0eaf9
Evgeni1494 <yoni1@hotmail.fr>
2023-01-16 14:01:24 +0100

    test ajout grille

--- exo.py
+++ exo.py
@@ -1,3 +1,5 @@
+import os
+
 def jouer_coup(plateau:dict, joueur:str, coup:str) -> None:
     """Fonction qui joue un coup (Ne vérifie pas si le coup est valide)
 
@@ -7,3 +9,22 @@ def jouer_coup(plateau:dict, joueur:str, coup:str) -> None:
         coup (str): Coordonnées de la forme "A1"
     """
     plateau[coup[0].upper()][int(coup[1])] = joueurs
+    
+
+def afficher_grille(plateau:dict) -> None:
+    """Fonction qui affiche la grille du morpion
+
+    Args:
+        plateau (dict): Un plateau de jeu
+    """
+    os.system("clear")
+    print(" \t|\t0\t|\t1\t|\t2\t|")
+    print("---------------------------------------------------------")
+    for cle in plateau:
+        print(cle, end="\t|\t")
+        for elt in plateau[cle]:
+            if elt == None:
+                print(" ", end="\t|\t")
+            else:
+                print(elt, end="\t|\t")
+        print("\n---------------------------------------------------------")    
